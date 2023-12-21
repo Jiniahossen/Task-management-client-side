a531db8d806bf3778ca5afa5f4b6c5fb7b456725
jinia hossen <jiniahossen31@gmail.com>
2023-12-21 20:11:52 +0600

    task display by user email

--- src/Pages/Dashboard/AddTask/AddTask.jsx
+++ src/Pages/Dashboard/AddTask/AddTask.jsx
@@ -1,43 +1,126 @@
-
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import { useForm } from "react-hook-form";
+import "react-datepicker/dist/react-datepicker.css";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import Swal from "sweetalert2";
 
 const AddTask = () => {
+    const axiosPublic=useAxiosPublic();
+    const {user}=useAuth();
+    const email=user?.email;
+    const {
+        register,
+        handleSubmit,
+        setValue, // Add this line
+    } = useForm();
+
+    const [startDate, setStartDate] = useState(new Date());
+
+    const onSubmit = (data) => {
+        // Include the email in the data object
+        const formData = {
+            ...data,
+            email: email,
+            list:'todo'
+        };
+    
+        axiosPublic.post('/tasks',formData)
+        .then((res)=>{
+            console.log(res.data);
+           if(res.data.insertedId){
+            Swal.fire('Task added successfully!')
+           }
+        })
+    };
+
     return (
         <div className="p-20">
-
-            <h1 className="text-4xl font-serif uppercase text-center mb-10">Add your task</h1>
+            <h1 className="text-4xl font-serif uppercase text-center mb-10">
+                Add your task
+            </h1>
             <div>
-
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="grid gap-6 mb-6 md:grid-cols-2">
                         <div>
-                            <label htmlFor="first_name" className="block font-serif mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-                            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Title" required />
+                            <label
+                                htmlFor="title"
+                                className="block font-serif mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                                Title
+                            </label>
+                            <input
+                                {...register("title", { required: true })}
+                                type="text"
+                                id="title"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Title"
+                                required
+                            />
                         </div>
                         <div>
-                            <label htmlFor="message" className="block mb-2 font-serif text-sm font-medium text-gray-900 dark:text-white">Your message</label>
-                            <textarea id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your task description..."></textarea>
-
+                            <label
+                                htmlFor="message"
+                                className="block mb-2 font-serif text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                                Your message
+                            </label>
+                            <textarea
+                                {...register("message", { required: true })}
+                                id="message"
+                                rows="4"
+                                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Write your task description..."
+                            ></textarea>
                         </div>
-
                     </div>
                     <div className="grid gap-6 mb-6 md:grid-cols-2">
                         <div>
-                            <label htmlFor="countries" className="block font-serif mb-2 text-sm font-medium text-gray-900 dark:text-white">Priority</label>
-                            <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                <option selected disabled>select priority</option>
-                                <option value="US">Low</option>
-                                <option value="CA">Moderate</option>
-                                <option value="FR">High</option>
+                            <label
+                                htmlFor="priority"
+                                className="block font-serif mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                                Priority
+                            </label>
+                            <select
+                                {...register("priority", { required: true })}
+                                id="priority"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            >
+                                <option value="" disabled>
+                                    Select priority
+                                </option>
+                                <option value="Low">Low</option>
+                                <option value="Moderate">Moderate</option>
+                                <option value="High">High</option>
                             </select>
                         </div>
                         <div>
-                            <label htmlFor="first_name" className="block font-serif mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Time</label>
-                            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Date" required />
+                            <label
+                                htmlFor="date"
+                                className="block font-serif mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                                Select Time
+                            </label>
+                            <DatePicker
+                                {...register("date", { required: true })}
+                                selected={startDate}
+                                onChange={(date) => {
+                                    setStartDate(date);
+                                    setValue("date", date, { shouldValidate: true });
+                                }}
+                                dateFormat="dd/MM/yyyy"
+                            />
                         </div>
                     </div>
-                    <button type="submit" className="text-white font-serif mt-10 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                    <button
+                        type="submit"
+                        className="font-serif mt-10 bg-[#67deae] text-black hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
+                    >
+                        Submit
+                    </button>
                 </form>
-
             </div>
         </div>
     );
--- src/Pages/Dashboard/AllTask/AllTask.jsx
+++ src/Pages/Dashboard/AllTask/AllTask.jsx
@@ -1,9 +1,92 @@
-
+import useTask from "../../../hooks/useTask";
+import { MdDeleteOutline } from "react-icons/md";
+import { FaRegEdit } from "react-icons/fa";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import Swal from "sweetalert2";
 
 const AllTask = () => {
+    const [task, refetch] = useTask();
+    const axiosPublic = useAxiosPublic();
+
+    const formatDate = (dateString) => {
+        const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+        return new Date(dateString).toLocaleDateString(undefined, options);
+    };
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            console.log(result);
+            if (result.isConfirmed === true) {
+                axiosPublic.delete(`/tasks/${id}`)
+                .then((res) => {
+                    if (res.data.success) {
+                        refetch();
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "Task has been deleted.",
+                            icon: "success"
+                        });
+                    } else {
+                        Swal.fire({
+                            title: "Error!",
+                            text: "Unable to delete the task.",
+                            icon: "error"
+                        });
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error deleting task:", error);
+                    Swal.fire({
+                        title: "Error!",
+                        text: "Unable to delete the task. Please try again later.",
+                        icon: "error"
+                    });
+                });            
+
+            }
+        });
+    }
     return (
         <div>
-            
+            <div className="flex gap-10 p-10">
+                <div className="flex-1">
+                    <h1 className="text-xl font-serif text-center mb-6">To do list:</h1>
+                    <div className="grid grid-cols-1 gap-6">
+                        {task?.map((tasks) => (
+                            <div key={tasks._id} className="shadow-md px-4 py-4 rounded-md">
+                                <h1 className="text-lg font-serif mt-2">{tasks.title}</h1>
+                                <p className="text-sm font-serif">{tasks.message}</p>
+                                <div className="flex gap-4">
+                                    <h1 className="text-base font-serif bg-[#67deae] w-20 text-center px-2 rounded-md mt-2 text-white">
+                                        {tasks.priority}
+                                    </h1>
+                                    <h1 className="text-base font-serif bg-red-600 text-center px-2 rounded-md mt-2 text-white">
+                                        {formatDate(tasks.date)}
+                                    </h1>
+                                </div>
+                                <div className="flex justify-between text-xl items-center mt-4">
+                                    <MdDeleteOutline onClick={() => handleDelete(tasks._id)} className="text-2xl text-red-600 cursor-pointer"></MdDeleteOutline>
+                                    <FaRegEdit className="text-green-500 cursor-pointer"></FaRegEdit>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="flex-1 ">
+                    <h1 className="text-xl text-center font-serif mb-6">Ongoing:</h1>
+                </div>
+                <div className="flex-1">
+                    <h1 className="text-xl  text-center font-serif mb-6">Completed:</h1>
+                </div>
+            </div>
         </div>
     );
 };
