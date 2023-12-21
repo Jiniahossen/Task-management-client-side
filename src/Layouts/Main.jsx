e33a179655f8919370570eb1b03c4c5cd84c2553
jinia hossen <jiniahossen31@gmail.com>
2023-12-21 20:27:05 +0600

    footer added and display to do list

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,16 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
 import Container from "../Components/Container/Container";
+import Footer from "../Components/Footer/Footer";
 
 
 const Main = () => {
     return (
-        <Container>
-            <div>
-                <Navbar></Navbar>
-                <Outlet></Outlet>
-            </div>
-        </Container>
+        <div>
+            <Container>
+                <div>
+                    <Navbar></Navbar>
+                    <Outlet></Outlet>
+                </div>
+            </Container>
+            <Footer></Footer>
+        </div>
     );
 };
 
